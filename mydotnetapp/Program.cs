85dc7b8a58212bd8f0a1465f300c16a97a5aafb6
Anshul Dahiya <anshul.dahiya@ihsmarkit.com>
2024-01-29 15:52:17 +0530

    multiple changes

--- mydotnetapp/Program.cs
+++ mydotnetapp/Program.cs
@@ -1,5 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Security.Cryptography;
+
+Console.WriteLine("Hello, World");
+
+var osNameAndVersion = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
+
+Console.WriteLine("OS : " + osNameAndVersion);
+
+
+string msg1 = "This is a Windows operating system.";
+string msg2 = "This is a Unix operating system.";
+string msg3 = "ERROR: This platform identifier is invalid.";
+
+switch (Environment.OSVersion.Platform)
+{
+    case PlatformID.Win32NT:
+    case PlatformID.Win32S:
+    case PlatformID.Win32Windows:
+    case PlatformID.WinCE:
+        Console.WriteLine(msg1);
+        break;
+    case PlatformID.Unix:
+        Console.WriteLine(msg2);
+        break;
+    default:
+        Console.WriteLine(msg3);
+        break;
+}
+
+var version = Environment.GetEnvironmentVariable("web_version");
+Console.WriteLine("Web Version is : " + version);
 
 string currfolder = System.IO.Directory.GetCurrentDirectory(); // @"c:\test\Test_C.txt";
 string fullPath = currfolder + "\\aaalog.txt";
