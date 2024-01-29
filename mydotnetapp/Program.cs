37f600e0ba07703dccbdc77499ea62c5a4352660
Anshul Dahiya <anshul.dahiya@ihsmarkit.com>
2024-01-29 15:55:41 +0530

    Refactoring

--- mydotnetapp/Program.cs
+++ mydotnetapp/Program.cs
@@ -4,36 +4,44 @@ using System.Security.Cryptography;
 Console.WriteLine("Hello, World");
 
 var osNameAndVersion = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
-
 Console.WriteLine("OS : " + osNameAndVersion);
 
-
-string msg1 = "This is a Windows operating system.";
-string msg2 = "This is a Unix operating system.";
-string msg3 = "ERROR: This platform identifier is invalid.";
-
-switch (Environment.OSVersion.Platform)
-{
-    case PlatformID.Win32NT:
-    case PlatformID.Win32S:
-    case PlatformID.Win32Windows:
-    case PlatformID.WinCE:
-        Console.WriteLine(msg1);
-        break;
-    case PlatformID.Unix:
-        Console.WriteLine(msg2);
-        break;
-    default:
-        Console.WriteLine(msg3);
-        break;
-}
+GetPlatform();
 
 var version = Environment.GetEnvironmentVariable("web_version");
 Console.WriteLine("Web Version is : " + version);
 
-string currfolder = System.IO.Directory.GetCurrentDirectory(); // @"c:\test\Test_C.txt";
-string fullPath = currfolder + "\\aaalog.txt";
-using (StreamWriter writer = new StreamWriter(fullPath, true))
+WriteToFile();
+
+static void GetPlatform()
 {
-    writer.WriteLine("Test value");
+    string msg1 = "This is a Windows operating system.";
+    string msg2 = "This is a Unix operating system.";
+    string msg3 = "ERROR: This platform identifier is invalid.";
+
+    switch (Environment.OSVersion.Platform)
+    {
+        case PlatformID.Win32NT:
+        case PlatformID.Win32S:
+        case PlatformID.Win32Windows:
+        case PlatformID.WinCE:
+            Console.WriteLine(msg1);
+            break;
+        case PlatformID.Unix:
+            Console.WriteLine(msg2);
+            break;
+        default:
+            Console.WriteLine(msg3);
+            break;
+    }
+}
+
+static void WriteToFile()
+{
+    string currfolder = System.IO.Directory.GetCurrentDirectory(); // @"c:\test\Test_C.txt";
+    string fullPath = currfolder + "\\aaalog.txt";
+    using (StreamWriter writer = new StreamWriter(fullPath, true))
+    {
+        writer.WriteLine("Test value");
+    }
 }
